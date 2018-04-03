f8bedebb7013161f5f6294d856ee0bc0d6daabf5
tangzhi <tangzhi@ctirobot.com>
2018-04-03 15:55:05 +0800

    commit project

--- CreateQRcode/ViewController.m
+++ CreateQRcode/ViewController.m
@@ -7,15 +7,101 @@
 //
 
 #import "ViewController.h"
+#import <CoreImage/CoreImage.h>
 
 @implementation ViewController
-
+{
+    __weak IBOutlet NSTextField *data;
+    __weak IBOutlet NSImageView *showImg;
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
 }
+- (IBAction)createQRcode:(id)sender {
+    [self create];
+}
 
+- (void)create {
+    // 1. 创建一个二维码滤镜实例(CIFilter)
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    // 滤镜恢复默认设置
+    [filter setDefaults];
+    
+    // 2. 给滤镜添加数据
+    NSString *string = data.stringValue;
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    // 使用KVC的方式给filter赋值
+    [filter setValue:data forKeyPath:@"inputMessage"];
+
+    // 3. 生成二维码
+    CIImage *image = [filter outputImage];
+    NSImage *img = [self createNonInterpolatedUIImageFormCIImage:image withSize:175];
+    showImg.image = img;
+}
+
+- (NSImage*)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size {
+    
+    CGRect extent = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    
+    // 1.创建bitmap;
+    size_t width = CGRectGetWidth(extent) * scale;
+    size_t height = CGRectGetHeight(extent) * scale;
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+    
+    // 2.保存bitmap到图片
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [self imageFromCGImageRef:scaledImage];
+}
+
+- (NSImage*) imageFromCGImageRef:(CGImageRef)image
+{
+    NSRect imageRect = NSMakeRect(0.0, 0.0, 0, 0);
+    CGContextRef imageContext = nil;
+    NSImage* newImage = nil;
+    
+    // Get the image dimensions.
+    imageRect.size.height = CGImageGetHeight(image);
+    imageRect.size.width = CGImageGetWidth(image);
+    
+    // Create a new image to receive the Quartz image data.
+    newImage = [[NSImage alloc] initWithSize:imageRect.size];
+    [newImage lockFocus];
+    
+    // Get the Quartz context and draw.
+    imageContext = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
+    CGContextDrawImage(imageContext, *(CGRect*)&imageRect, image);
+    [newImage unlockFocus];
+    return newImage;
+}
+
+- (IBAction)save:(id)sender {
+    NSSavePanel *panel = [NSSavePanel savePanel];
+    [panel setNameFieldStringValue:@"Untitle"];
+    [panel setMessage:@"保存二维码图片"];
+    [panel setAllowsOtherFileTypes:YES];
+    [panel setAllowedFileTypes:@[@"png",@"jpg"]];
+    [panel setExtensionHidden:YES];
+    [panel setCanCreateDirectories:YES];
+    [panel beginSheetModalForWindow:self.view.window completionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSString *path = [[panel URL] path];
+            NSData *picData = [showImg.image TIFFRepresentation];
+            [picData writeToFile:path atomically:YES];
+        }
+    }];
+}
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
